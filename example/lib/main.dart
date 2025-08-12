b54e5da507df842be8d6ec37993a9258d149fedc
OkoroaforKelechiDivine <okoroaforkelechi123@gmail.com>
2025-08-12 18:59:06 +0100

    feature: added 0.0.2 (check details in changelog.md)

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -11,26 +11,25 @@ class ExampleApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          leading: BackButton(),
-          title: const Text('EasyCarousel'),
+          leading: const BackButton(),
+          title: const Text('EasyCarousel Demo'),
+          backgroundColor: Colors.blueAccent,
         ),
         body: EasyCarousel(
           imageUrls: [
-            'https://picsum.photos/id/1011/600/400',
-            'https://picsum.photos/id/1012/600/400',
+            'assets/images/image1.jpg',
+            'assets/images/image2.jpg',
             'https://picsum.photos/id/1013/600/400',
           ],
           headlineTexts: ['Welcome', 'Explore', 'Enjoy'],
           captionTexts: [
-            'Welcome to EasyCarousel package demo',
-            'Swipe to explore features',
-            'Enjoy building carousels!',
+            'Discover the power of EasyCarousel!',
+            'Swipe to explore amazing features',
+            'Build stunning carousels with ease!',
           ],
           isNavigationButtonVisible: true,
-          activeIndicatorDotHeight: 5,
           continueButtonText: 'Next',
           completeButtonText: 'Get Started',
-          spaceBetweenImageAndText: 32,
           navigationButtonIcon: const Icon(
             Icons.arrow_forward,
             color: Colors.white,
@@ -41,6 +40,35 @@ class ExampleApp extends StatelessWidget {
           onCarouselComplete: () {
             debugPrint('You finished the carousel!');
           },
+          buttonColor: Colors.blueAccent,
+          buttonTextColor: Colors.white,
+          imageWidth: 0.9,
+          imageHeight: 300.0,
+          headlineFontSize: 24.0,
+          captionFontSize: 16.0,
+          headlineFontColor: Colors.black87,
+          captionFontColor: Colors.grey[600]!,
+          imageSourceType: ImageSourceType.asset,
+          activeIndicatorDotColor: Colors.blueAccent,
+          inactiveIndicatorDotColor: Colors.grey[300]!,
+          activeIndicatorDotWidth: 24.0,
+          activeIndicatorDotHeight: 6.0,
+          inactiveIndicatorDotWidth: 12.0,
+          inactiveIndicatorDotHeight: 6.0,
+          indicatorDotSpacing: 10.0,
+          spaceBetweenImageAndText: 40.0,
+          headlineTextStyle: const TextStyle(
+            fontFamily: 'Roboto',
+            fontWeight: FontWeight.bold,
+          ),
+          captionTextStyle: const TextStyle(
+            fontFamily: 'Roboto',
+            fontStyle: FontStyle.italic,
+          ),
+          navigationButtonTextStyle: const TextStyle(
+            fontSize: 16.0,
+            fontWeight: FontWeight.w600,
+          ),
         ),
       ),
     );
